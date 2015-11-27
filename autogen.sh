9c264cbb9fc24057285a94bca10562e49f17ccc8
Jamal Natour <Jamal.Natour@fumasoftware.com>
2015-11-27 00:49:21 +0000

    Initial import

---
+++ autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# workout the absolute path to the checkout directory
+abspath()
+{
+    case "${1}" in
+        [./]*)
+            local ABSPATH="$(cd ${1%/*}; pwd)/${1##*/}"
+            echo "${ABSPATH}"
+            ;;
+        *)
+            echo "${PWD}/${1}"
+            ;;
+    esac
+}
+
+SCRIPT=$(abspath ${0})
+ROOTPATH=`dirname ${SCRIPT}`
+export PROJECT_ROOT=${ROOTPATH}
+
+cd ${PROJECT_ROOT};
+
+autoreconf -fvi
