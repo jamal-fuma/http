40a69bc2156c5e7009f35f1c9626956bed2a2fbc
Jamal Natour <Jamal.Natour@fumasoftware.com>
2016-08-31 23:59:27 +0100

    Patch: use common test tooling

--- tests/src/test_headers.cpp
+++ tests/src/test_headers.cpp
@@ -6,40 +6,62 @@
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+
+#include "TestHelper.hpp"
+
 #include <ssiloti/http/parsers/headers.hpp>
-#include <boost/test/unit_test.hpp>
 #include <algorithm>
 
 using namespace http;
 
-template <typename Header>
-void test_token_map()
+struct FixtureData
+    : public Fuma::Test::Fixture
 {
-    Header h;
-    std::string testv_pass("token, extension-token = token-param, extension-token-q = \"quoted param\"");
-    BOOST_REQUIRE(parsers::parse_header(h, testv_pass.begin(), testv_pass.end()));
-    BOOST_CHECK(h.second.count("token"));
-    BOOST_CHECK_EQUAL(h.second["token"], std::string());
-    BOOST_CHECK(h.second.count("extension-token"));
-    BOOST_CHECK_EQUAL(h.second["extension-token"], std::string("token-param"));
-    BOOST_CHECK(h.second.count("extension-token-q"));
-    BOOST_CHECK_EQUAL(h.second["extension-token-q"], std::string("quoted param"));
+
+    template <typename Header>
+    void test_token_map()
+    {
+        Header h;
+        std::string testv_pass("token, extension-token = token-param, extension-token-q = \"quoted param\"");
+        BOOST_REQUIRE(parsers::parse_header(h, testv_pass.begin(), testv_pass.end()));
+        BOOST_CHECK(h.second.count("token"));
+        BOOST_CHECK_EQUAL(h.second["token"], std::string());
+        BOOST_CHECK(h.second.count("extension-token"));
+        BOOST_CHECK_EQUAL(h.second["extension-token"], std::string("token-param"));
+        BOOST_CHECK(h.second.count("extension-token-q"));
+        BOOST_CHECK_EQUAL(h.second["extension-token-q"], std::string("quoted param"));
+    }
+
+    template <typename Header>
+    void test_token_vector()
+    {
+        Header h;
+        std::string testv_pass("token1, token2, token3");
+        BOOST_REQUIRE(parsers::parse_header(h, testv_pass.begin(), testv_pass.end()));
+        BOOST_CHECK_EQUAL(h.second.size(), 3);
+        BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token1")) != h.second.end());
+        BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token2")) != h.second.end());
+        BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token3")) != h.second.end());
+    }
+
+};
+
+BOOST_FIXTURE_TEST_SUITE(BasicSuite, FixtureData)
+
+BOOST_AUTO_TEST_CASE(should_produce_correct_token_map)
+{
+    test_token_map<headers::cache_control>();
+    test_token_map<headers::pragma>();
 }
 
-template <typename Header>
-void test_token_vector()
+BOOST_AUTO_TEST_CASE(should_produce_correct_token_vector)
 {
-    Header h;
-    std::string testv_pass("token1, token2, token3");
-    BOOST_REQUIRE(parsers::parse_header(h, testv_pass.begin(), testv_pass.end()));
-    BOOST_CHECK_EQUAL(h.second.size(), 3);
-    BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token1")) != h.second.end());
-    BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token2")) != h.second.end());
-    BOOST_CHECK(std::find(h.second.begin(), h.second.end(), std::string("token3")) != h.second.end());
+    test_token_vector<headers::connection>();
+    test_token_vector<headers::trailer>();
+    test_token_vector<headers::allow>();
 }
 
-void test_date()
+BOOST_AUTO_TEST_CASE(should_produce_correct_date)
 {
     headers::date d;
     std::string testv_pass("Tue, 15 Nov 1994 08:12:31 GMT");
@@ -52,7 +74,7 @@ void test_date()
     BOOST_CHECK_EQUAL(d.second.time_of_day().seconds(), 31);
 }
 
-void test_transfer_encoding()
+BOOST_AUTO_TEST_CASE(should_produce_correct_transfer_encoding)
 {
     headers::transfer_encoding te;
     std::string testv_pass("token1, token2; param1=word, token3");
@@ -65,7 +87,7 @@ void test_transfer_encoding()
     BOOST_CHECK_EQUAL(te.second["token2"]["param1"], std::string("word"));
 }
 
-void test_upgrade()
+BOOST_AUTO_TEST_CASE(should_produce_correct_upgrade)
 {
     headers::upgrade u;
     std::string testv_pass("product1, product2/version2, product3");
@@ -79,7 +101,7 @@ void test_upgrade()
     BOOST_CHECK_EQUAL(u.second[2].version.size(), 0);
 }
 
-void test_via()
+BOOST_AUTO_TEST_CASE(should_produce_correct_via)
 {
     headers::via v;
     std::string testv_pass("1.0 fred, 1.1 p.example.net (Apache/1.1)");
@@ -95,7 +117,7 @@ void test_via()
     BOOST_CHECK_EQUAL(boost::get<std::string>(v.second[1].comment[0]), std::string("Apache/1.1"));
 }
 
-void test_warning()
+BOOST_AUTO_TEST_CASE(should_produce_correct_warning)
 {
     headers::warning w;
     std::string testv_pass("123 p.example.net:1 \"danger\", 111 t.example.net \"will\" \"Tue, 15 Nov 1994 08:12:31 GMT\"");
@@ -118,7 +140,7 @@ void test_warning()
     BOOST_CHECK_EQUAL(w.second[1].date->time_of_day().seconds(), 31);
 }
 
-void test_accept()
+BOOST_AUTO_TEST_CASE(should_produce_correct_accept)
 {
     headers::accept a;
     std::string testv_pass("audio/*; q=0.2, audio/basic");
@@ -132,7 +154,7 @@ void test_accept()
     BOOST_CHECK(!a.second[1].params);
 }
 
-void test_accept_charset()
+BOOST_AUTO_TEST_CASE(should_produce_correct_accept_charset)
 {
     headers::accept_charset ac;
     std::string testv_pass("iso-8859-5, unicode-1-1;q=0.8");
@@ -144,7 +166,7 @@ void test_accept_charset()
     BOOST_CHECK_EQUAL(ac.second[1].qvalue.get(), 0.8f);
 }
 
-void test_accept_encoding()
+BOOST_AUTO_TEST_CASE(should_produce_correct_accept_encoding)
 {
     headers::accept_encoding ae;
     std::string testv_pass("compress, gzip;q=1.0");
@@ -156,7 +178,7 @@ void test_accept_encoding()
     BOOST_CHECK_EQUAL(ae.second[1].qvalue.get(), 1.0f);
 }
 
-void test_accept_language()
+BOOST_AUTO_TEST_CASE(should_produce_correct_accept_language)
 {
     headers::accept_language al;
     std::string testv_pass("da, en-gb;q=0.8, en;q=0.7");
@@ -170,7 +192,7 @@ void test_accept_language()
     BOOST_CHECK_EQUAL(al.second[2].qvalue.get(), 0.7f);
 }
 
-void test_authorization()
+BOOST_AUTO_TEST_CASE(should_produce_correct_authorization)
 {
     headers::authorization a;
     std::string testv_pass("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
@@ -179,7 +201,7 @@ void test_authorization()
     BOOST_CHECK_EQUAL(boost::get<std::string>(a.second.params.get()), std::string("QWxhZGRpbjpvcGVuIHNlc2FtZQ=="));
 }
 
-void test_expect()
+BOOST_AUTO_TEST_CASE(should_produce_correct_expect)
 {
     headers::expect e;
     std::string testv_pass("100-continue, extention = value; param=value");
@@ -191,7 +213,7 @@ void test_expect()
     BOOST_CHECK_EQUAL(boost::fusion::at_c<1>(e.second["extention"])["param"], std::string("value"));
 }
 
-void test_host()
+BOOST_AUTO_TEST_CASE(should_produce_correct_host)
 {
     headers::host h;
     std::string testv_pass("www.example.org");
@@ -199,7 +221,7 @@ void test_host()
     BOOST_CHECK_EQUAL(h.second.host, std::string("www.example.org"));
 }
 
-void test_if_match()
+BOOST_AUTO_TEST_CASE(should_produce_correct_if_match)
 {
     headers::if_match ifm;
     std::string testv_pass("W/\"xyzzy\", \"r2d2xxxx\", \"c3piozzzz\"");
@@ -212,7 +234,7 @@ void test_if_match()
     BOOST_CHECK_EQUAL(boost::get<std::vector<http::entity_tag> >(ifm.second)[2].tag, std::string("c3piozzzz"));
 }
 
-void test_content_length()
+BOOST_AUTO_TEST_CASE(should_produce_correct_content_length)
 {
     headers::content_length cl;
     std::string testv_pass("1234");
@@ -223,37 +245,11 @@ void test_content_length()
     BOOST_CHECK(!parsers::parse_header(cl, testv_fail.begin(), testv_fail.end()));
 }
 
-void test_content_type()
+BOOST_AUTO_TEST_CASE(should_produce_correct_content_type)
 {
     headers::content_type ct;
     std::string testv_pass("text/html; charset=ISO-8859-4");
     BOOST_REQUIRE(parsers::parse_header(ct, testv_pass.begin(), testv_pass.end()));
 }
 
-void init_headers_suite(int, char * [])
-{
-    boost::unit_test::test_suite * test = BOOST_TEST_SUITE("headers");
-    test->add(BOOST_TEST_CASE(&test_token_map<headers::cache_control>));
-    test->add(BOOST_TEST_CASE(&test_token_map<headers::pragma>));
-    test->add(BOOST_TEST_CASE(&test_token_vector<headers::connection>));
-    test->add(BOOST_TEST_CASE(&test_token_vector<headers::trailer>));
-    test->add(BOOST_TEST_CASE(&test_token_vector<headers::allow>));
-    test->add(BOOST_TEST_CASE(&test_date));
-    test->add(BOOST_TEST_CASE(&test_transfer_encoding));
-    test->add(BOOST_TEST_CASE(&test_upgrade));
-    test->add(BOOST_TEST_CASE(&test_via));
-    test->add(BOOST_TEST_CASE(&test_warning));
-    test->add(BOOST_TEST_CASE(&test_accept));
-    test->add(BOOST_TEST_CASE(&test_accept_charset));
-    test->add(BOOST_TEST_CASE(&test_accept_encoding));
-    test->add(BOOST_TEST_CASE(&test_accept_language));
-    test->add(BOOST_TEST_CASE(&test_authorization));
-    test->add(BOOST_TEST_CASE(&test_expect));
-    test->add(BOOST_TEST_CASE(&test_host));
-    test->add(BOOST_TEST_CASE(&test_if_match));
-    test->add(BOOST_TEST_CASE(&test_content_length));
-    test->add(BOOST_TEST_CASE(&test_content_type));
-
-    boost::unit_test::framework::master_test_suite().add(test);
-}
-
+BOOST_AUTO_TEST_SUITE_END()
